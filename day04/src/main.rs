1c655112d3b22fc50c67dcaff83c55024d5e546c
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-04 09:23:29 +0100

    Day 4: part two

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -3,18 +3,18 @@ use std::collections::HashSet;
 use regex::Regex;
 
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 fn run(input: &'static str) -> (usize, usize) {
     let re = Regex::new(r"Card +(?<id>\d+): (?<winning>[\d ]+) \| (?<own>[\d ]+)").unwrap();
+    let mut num_cards: Vec<usize> = vec![1; input.lines().count()];
 
     let mut part1_answer = 0;
-    for line in input.lines() {
+    for (i, line) in input.lines().enumerate() {
         for cap in re.captures_iter(line) {
-            let _id = cap["id"].parse::<usize>().unwrap();
             let winning: HashSet<_> = cap["winning"]
                 .split_ascii_whitespace()
                 .map(|x| x.parse::<usize>().unwrap())
@@ -23,14 +23,16 @@ fn run(input: &'static str) -> (usize, usize) {
                 .split_ascii_whitespace()
                 .map(|x| x.parse::<usize>().unwrap())
                 .collect();
-            let num_winning = winning.intersection(&own).count() as u32;
+            let num_winning = winning.intersection(&own).count();
             if num_winning > 0 {
-                part1_answer += 2usize.pow(num_winning - 1);
+                part1_answer += 2usize.pow((num_winning - 1) as u32);
+            }
+            for j in 1..=num_winning {
+                num_cards[i + j] += num_cards[i];
             }
         }
     }
-
-    let part2_answer = 0;
+    let part2_answer = num_cards.iter().sum();
     (part1_answer, part2_answer)
 }
 
@@ -40,15 +42,15 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 23750);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 13261850);
     }
 
     #[test]
     fn test_input_example() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 13);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 30);
     }
 }
