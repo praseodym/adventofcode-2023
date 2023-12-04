5877a00d9ef04500f4b28c5d910a41a95e279c89
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-04 18:20:26 +0100

    Day 4: cleanup

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,7 +1,5 @@
 use std::collections::HashSet;
 
-use regex::Regex;
-
 fn main() {
     let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
@@ -9,27 +7,26 @@ fn main() {
 }
 
 fn run(input: &'static str) -> (usize, usize) {
-    let re = Regex::new(r"Card +(?<id>\d+): (?<winning>[\d ]+) \| (?<own>[\d ]+)").unwrap();
     let mut num_cards: Vec<usize> = vec![1; input.lines().count()];
 
     let mut part1_answer = 0;
     for (i, line) in input.lines().enumerate() {
-        for cap in re.captures_iter(line) {
-            let winning: HashSet<_> = cap["winning"]
-                .split_ascii_whitespace()
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect();
-            let own: HashSet<_> = cap["own"]
-                .split_ascii_whitespace()
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect();
-            let num_winning = winning.intersection(&own).count();
-            if num_winning > 0 {
-                part1_answer += 2usize.pow((num_winning - 1) as u32);
-            }
-            for j in 1..=num_winning {
-                num_cards[i + j] += num_cards[i];
-            }
+        let i1 = line.find(':').unwrap();
+        let i2 = line.find('|').unwrap();
+        let winning: HashSet<_> = line[i1 + 1..i2]
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        let own: HashSet<_> = line[i2 + 1..]
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        let num_winning = winning.intersection(&own).count();
+        if num_winning > 0 {
+            part1_answer += 2usize.pow((num_winning - 1) as u32);
+        }
+        for j in 1..=num_winning {
+            num_cards[i + j] += num_cards[i];
         }
     }
     let part2_answer = num_cards.iter().sum();
