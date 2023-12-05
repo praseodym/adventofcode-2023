b46d952d6d97972ec6a47e848d31368c76516c28
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-05 06:45:28 +0100

    Day 5: part two

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,9 +1,9 @@
 use std::cmp;
 
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -15,6 +15,7 @@ struct Mapping {
 
 fn run(input: &'static str) -> (usize, usize) {
     let mut seeds: Vec<usize> = vec![];
+    let mut seeds_range: Vec<(usize, usize)> = vec![];
     let mut maps: Vec<Vec<Mapping>> = vec![];
     for block in input.split("\n\n") {
         let mut s = block.split(':');
@@ -25,6 +26,9 @@ fn run(input: &'static str) -> (usize, usize) {
                 .split_ascii_whitespace()
                 .map(|x| x.parse::<usize>().unwrap())
                 .collect();
+            for i in (0..seeds.len()).step_by(2) {
+                seeds_range.push((seeds[i], seeds[i + 1]));
+            }
         } else {
             let mut map: Vec<Mapping> = vec![];
             for line in content.lines() {
@@ -40,20 +44,32 @@ fn run(input: &'static str) -> (usize, usize) {
 
     let mut part1_answer = usize::MAX;
     for seed in seeds.iter() {
-        let mut x = *seed;
-        for map in &maps {
-            for mapping in map {
-                if mapping.src <= x && mapping.src + mapping.length > x {
-                    x = x - mapping.src + mapping.dest;
-                    break;
-                }
-            }
-        }
-        part1_answer = cmp::min(part1_answer, x);
+        let location = map_seed_to_location(&maps, *seed);
+        part1_answer = cmp::min(part1_answer, location);
     }
 
-    let part2_answer = 0;
+    // Inefficient but works
+    let mut part2_answer = usize::MAX;
+    for (a, r) in seeds_range.iter() {
+        for seed in *a..*a + *r {
+            let location = map_seed_to_location(&maps, seed);
+            part2_answer = cmp::min(part2_answer, location);
+        }
+    }
     (part1_answer, part2_answer)
+}
+
+fn map_seed_to_location(maps: &Vec<Vec<Mapping>>, seed: usize) -> usize {
+    let mut x = seed;
+    for map in maps {
+        for mapping in map {
+            if mapping.src <= x && mapping.src + mapping.length > x {
+                x = x - mapping.src + mapping.dest;
+                break;
+            }
+        }
+    }
+    x
 }
 
 #[cfg(test)]
@@ -62,15 +78,15 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 388071289);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 84206669);
     }
 
     #[test]
     fn test_input_example() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 35);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 46);
     }
 }
