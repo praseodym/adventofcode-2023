757d6a41b791f9bd9e134adbe0f404bfb6836243
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-07 12:17:36 +0100

    Day 7: cleanup

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 fn main() {
     let (part1_answer, part2_answer) = run(include_str!("../input"));
@@ -72,42 +73,38 @@ impl Card {
 }
 
 impl HandType {
-    fn get_type(card: &[Card]) -> HandType {
-        let mut card_count = std::collections::HashMap::new();
-        for c in card {
-            let count = card_count.entry(c.clone()).or_insert(0);
-            *count += 1;
-        }
-        let mut card_count: Vec<(Card, usize)> = card_count.into_iter().collect();
-        card_count.sort_by(|a, b| b.1.cmp(&a.1));
+    fn get_type(cards: &[Card]) -> HandType {
+        let mut card_count = cards.iter().fold(HashMap::new(), |mut counts, card| {
+            *counts.entry(card.clone()).or_insert(0) += 1;
+            counts
+        });
+        let joker_count = card_count.remove(&Card::Joker).unwrap_or(0);
         let mut card_count: Vec<usize> = card_count.into_iter().map(|x| x.1).collect();
-        card_count.sort_by(|a, b| b.cmp(&a));
-        let jokers = card.iter().filter(|x| **x == Card::Joker).count();
-        match (jokers, card_count.as_slice()) {
+        card_count.sort_by(|a, b| b.cmp(a));
+        match (joker_count, card_count.as_slice()) {
             (0, [5]) => HandType::FiveOfAKind,
-            (1, [4, 1]) => HandType::FiveOfAKind,
-            (2, [3, 2]) => HandType::FiveOfAKind,
-            (3, [3, 2]) => HandType::FiveOfAKind,
-            (4, [4, 1]) => HandType::FiveOfAKind,
-            (5, [5]) => HandType::FiveOfAKind,
+            (1, [4]) => HandType::FiveOfAKind,
+            (2, [3]) => HandType::FiveOfAKind,
+            (3, [2]) => HandType::FiveOfAKind,
+            (4, [1]) => HandType::FiveOfAKind,
+            (5, []) => HandType::FiveOfAKind,
             (0, [4, 1]) => HandType::FourOfAKind,
-            (1, [3, 1, 1]) => HandType::FourOfAKind,
-            (2, [2, 2, 1]) => HandType::FourOfAKind,
-            (3, [3, 1, 1]) => HandType::FourOfAKind,
+            (1, [3, 1]) => HandType::FourOfAKind,
+            (2, [2, 1]) => HandType::FourOfAKind,
+            (3, [1, 1]) => HandType::FourOfAKind,
             (0, [3, 2]) => HandType::FullHouse,
-            (1, [2, 2, 1]) => HandType::FullHouse,
+            (1, [2, 2]) => HandType::FullHouse,
             (0, [3, 1, 1]) => HandType::ThreeOfAKind,
-            (1, [2, 1, 1, 1]) => HandType::ThreeOfAKind,
-            (2, [2, 1, 1, 1]) => HandType::ThreeOfAKind,
+            (1, [2, 1, 1]) => HandType::ThreeOfAKind,
+            (2, [1, 1, 1]) => HandType::ThreeOfAKind,
             (0, [2, 2, 1]) => HandType::TwoPair,
             (0, [2, 1, 1, 1]) => HandType::OnePair,
-            (1, [1, 1, 1, 1, 1]) => HandType::OnePair,
+            (1, [1, 1, 1, 1]) => HandType::OnePair,
             (0, [1, 1, 1, 1, 1]) => HandType::HighCard,
             _ => HandType::None,
         }
     }
 }
-
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -145,12 +142,11 @@ fn calculate_winnings(input: &str, joker: bool) -> usize {
             let card = Card::from_str(&card, joker);
             let bid = s.next().unwrap().parse::<usize>().unwrap();
             let hand_type = HandType::get_type(&card);
-            let hand = Hand {
+            Hand {
                 card,
                 bid,
                 hand_type,
-            };
-            hand
+            }
         })
         .collect();
 
@@ -159,9 +155,7 @@ fn calculate_winnings(input: &str, joker: bool) -> usize {
     hands
         .iter()
         .enumerate()
-        .map(|(i, hand)| {
-            (i + 1) * hand.bid
-        })
+        .map(|(i, hand)| (i + 1) * hand.bid)
         .sum()
 }
 
