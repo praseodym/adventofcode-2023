98e0652bf1bf79213d05cdb01279539ecd10efd5
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-09 10:16:04 +0100

    Day 9: cleanup

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -15,28 +15,29 @@ fn run(input: &'static str) -> (isize, isize) {
         let mut extrapolation: Vec<Vec<isize>> = vec![values];
         loop {
             let mut differences: Vec<isize> = vec![];
+            let mut sum = 0;
             let last = extrapolation.last().unwrap();
             for i in 1..last.len() {
-                differences.push(last[i] - last[i - 1]);
+                let d = last[i] - last[i - 1];
+                differences.push(d);
+                sum += d.abs();
             }
-            if differences.iter().all(|&x| x == 0) {
+            if sum == 0 {
                 break;
             }
             extrapolation.push(differences);
         }
+        extrapolation.reverse();
 
         part1_answer += extrapolation
             .iter()
-            .rev()
             .map(|x| *x.last().unwrap())
             .sum::<isize>();
         part2_answer += extrapolation
             .iter()
-            .rev()
             .map(|x| *x.first().unwrap())
             .fold(0, |acc, x| x - acc);
     }
-
     (part1_answer, part2_answer)
 }
 
