e6733e187c398ace1b8f4f217d1b33347a0bca7c
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-11 06:48:53 +0100

    Day 11: part two

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,10 +1,22 @@
+use std::cmp;
+use std::collections::HashSet;
+
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 fn run(input: &'static str) -> (usize, usize) {
+    let (grid, empty_rows, empty_columns) = parse_input(input);
+
+    let part1_answer = sum_of_distances(&grid, &empty_rows, &empty_columns, 1);
+    let part2_answer = sum_of_distances(&grid, &empty_rows, &empty_columns, 1000000 - 1);
+
+    (part1_answer, part2_answer)
+}
+
+fn parse_input(input: &str) -> (Vec<Vec<char>>, Vec<usize>, Vec<usize>) {
     let grid = input
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
@@ -34,31 +46,42 @@ fn run(input: &'static str) -> (usize, usize) {
             Some(x)
         })
         .collect::<Vec<_>>();
+    (grid, empty_rows, empty_columns)
+}
 
+fn sum_of_distances(
+    grid: &[Vec<char>],
+    empty_rows: &[usize],
+    empty_columns: &[usize],
+    expansion: usize,
+) -> usize {
     let mut galaxies = vec![];
     for (y, row) in grid.iter().enumerate() {
         for (x, c) in row.iter().enumerate() {
             if *c == '#' {
-                let dx = empty_columns.iter().filter(|&&ex| ex < x).count();
-                let dy = empty_rows.iter().filter(|&&ey| ey < y).count();
+                let dx = empty_columns.iter().filter(|&&ex| ex < x).count() * expansion;
+                let dy = empty_rows.iter().filter(|&&ey| ey < y).count() * expansion;
                 galaxies.push(((x + dx) as isize, (y + dy) as isize));
             }
         }
     }
 
-    let mut part1_answer = 0;
-    let mut distances = vec![vec![0; galaxies.len()]; galaxies.len()];
+    let mut sum = 0;
+    let mut seen = HashSet::new();
     for (i, (x1, y1)) in galaxies.iter().enumerate() {
         for (j, (x2, y2)) in galaxies.iter().enumerate() {
+            let a = cmp::min(i, j);
+            let b = cmp::max(i, j);
+            if seen.contains(&(a, b)) {
+                continue;
+            }
+            seen.insert((a, b));
+
             // manhattan distance between (x1, y1) and (x2, y2)
-            distances[i][j] = (x1 - x2).abs() + (y1 - y2).abs();
-            part1_answer += distances[i][j] as usize;
+            sum += ((x1 - x2).abs() + (y1 - y2).abs()) as usize;
         }
     }
-    part1_answer /= 2;
-
-    let part2_answer = 0;
-    (part1_answer, part2_answer)
+    sum
 }
 
 #[cfg(test)]
@@ -67,15 +90,25 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 9742154);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 411142919886);
     }
 
     #[test]
     fn test_input_example() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, _) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 374);
-        // assert_eq!(part2_answer, 0);
+    }
+
+    #[test]
+    fn test_input_example_expanded() {
+        let (grid, empty_rows, empty_columns) = parse_input(include_str!("../input-example"));
+
+        let sum = sum_of_distances(&grid, &empty_rows, &empty_columns, 10 - 1);
+        assert_eq!(sum, 1030);
+
+        let sum = sum_of_distances(&grid, &empty_rows, &empty_columns, 100 - 1);
+        assert_eq!(sum, 8410);
     }
 }
