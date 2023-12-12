ad8b1ab3548347c8d17273e1b39050a3f7dd6a1f
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-12 08:47:52 +0100

    Day 12: part two

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
+
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 fn run(input: &'static str) -> (usize, usize) {
     let mut part1_answer = 0;
+    let mut part2_answer = 0;
     for line in input.lines() {
         let mut s = line.split_ascii_whitespace();
         let record = s.next().unwrap();
@@ -15,34 +18,69 @@ fn run(input: &'static str) -> (usize, usize) {
             .split(',')
             .map(|x| x.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
-        part1_answer += brute_force(record, &group, 0);
+        part1_answer += solve(record, &group);
+        let record = format!("{}?{}?{}?{}?{}", record, record, record, record, record);
+        let group = group.repeat(5);
+        part2_answer += solve(&record, &group);
     }
-
-    let mut part2_answer = 0;
     (part1_answer, part2_answer)
 }
 
-fn brute_force(record: &str, group: &[usize], i: usize) -> usize {
-    let mut possibilities = 0;
-    if i == record.len() {
-        let option = record
-            .split('.')
-            .filter(|r| !r.is_empty())
-            .map(|r| r.len())
-            .collect::<Vec<_>>();
-        return if option == group { 1 } else { 0 };
+fn solve(record: &str, groups: &[usize]) -> usize {
+    return solve(
+        &record.chars().collect::<Vec<_>>(),
+        groups,
+        0,
+        0,
+        0,
+        &mut HashMap::new(),
+    );
+
+    fn solve(
+        record: &[char],
+        groups: &[usize],
+        ri: usize, // record index
+        gi: usize, // groups index
+        gl: usize, // length of group at gi
+        cache: &mut HashMap<(usize, usize, usize), usize>,
+    ) -> usize {
+        if let Some(&p) = cache.get(&(ri, gi, gl)) {
+            return p;
+        }
+        if ri == record.len() {
+            // record done
+            return if (gi == groups.len() && gl == 0)
+                || (gi == groups.len() - 1 && groups[gi] == gl)
+            {
+                // completed without no unfinished groups, so possible
+                1
+            } else {
+                // completed with unfinished groups, so impossible
+                0
+            };
+        }
+
+        let mut possibilities = 0;
+        let chars = if record[ri] == '?' {
+            vec!['#', '.']
+        } else {
+            vec![record[ri]]
+        };
+        for &c in chars.iter() {
+            if c == '#' {
+                // continue current group with #
+                possibilities += solve(record, groups, ri + 1, gi, gl + 1, cache);
+            } else if c == '.' && gl == 0 {
+                // continue current empty group with .
+                possibilities += solve(record, groups, ri + 1, gi, gl, cache);
+            } else if c == '.' && gi < groups.len() && groups[gi] == gl {
+                // current group done, start new group
+                possibilities += solve(record, groups, ri + 1, gi + 1, 0, cache);
+            }
+        }
+        cache.insert((ri, gi, gl), possibilities);
+        possibilities
     }
-    if record.chars().nth(i).unwrap() == '?' {
-        let mut record = record.to_string();
-        record.replace_range(i..i + 1, ".");
-        possibilities += brute_force(&record, group, i + 1);
-        let mut record = record.to_string();
-        record.replace_range(i..i + 1, "#");
-        possibilities += brute_force(&record, group, i + 1);
-    } else {
-        possibilities += brute_force(record, group, i + 1);
-    }
-    possibilities
 }
 
 #[cfg(test)]
@@ -51,15 +89,33 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 7653);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 60681419004564);
     }
 
     #[test]
     fn test_input_example() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 21);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 525152);
+    }
+
+    #[test]
+    fn test_simple() {
+        let p = solve("#.#.###", &[1, 1, 3]);
+        assert_eq!(p, 1);
+        let p = solve("?.#.###", &[1, 1, 3]);
+        assert_eq!(p, 1);
+        let p = solve("??#.###", &[1, 1, 3]);
+        assert_eq!(p, 1);
+        let p = solve("???.###", &[1, 1, 3]);
+        assert_eq!(p, 1);
+    }
+
+    #[test]
+    fn test_multiple() {
+        let p = solve(".??..??...?##.", &[1, 1, 3]);
+        assert_eq!(p, 4);
     }
 }
