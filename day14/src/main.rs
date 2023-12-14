df665b34fbed4224fb7225c14a094b3582102494
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-14 21:53:32 +0100

    Day 14: simplify

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -20,27 +20,26 @@ fn run(input: &'static str) -> (usize, usize) {
         }
         max_y = max_y.max(y);
     }
+    assert_eq!(max_x, max_y);
 
-    tilt_north(&mut platform, max_x, max_y);
+    let part1_answer = {
+        tilt_north(&mut platform, max_x, max_y);
+        calculate_load(&platform, max_y)
+    };
 
-    let mut part1_answer = 0;
     let mut part2_answer = 0;
-
     let mut seen = Vec::new();
     for i in 0..RUNS {
-        tilt_north(&mut platform, max_x, max_y);
-        if part1_answer == 0 {
-            part1_answer = calculate_load(&platform, max_x, max_y);
+        for _ in 0..4 {
+            tilt_north(&mut platform, max_x, max_y);
+            platform = rotate(&platform, max_x, max_y);
         }
-        tilt_west(&mut platform, max_x, max_y);
-        tilt_south(&mut platform, max_x, max_y);
-        tilt_east(&mut platform, max_x, max_y);
 
         if seen.contains(&platform) {
             let cycle_start = seen.iter().position(|&p| p == platform).unwrap();
             let cycle_length = seen.len() - cycle_start;
             let cycle_index = (RUNS - i - 1).rem(cycle_length);
-            part2_answer = calculate_load(&seen[cycle_start + cycle_index], max_x, max_y);
+            part2_answer = calculate_load(&seen[cycle_start + cycle_index], max_y);
             break;
         }
 
@@ -48,18 +47,6 @@ fn run(input: &'static str) -> (usize, usize) {
     }
 
     (part1_answer, part2_answer)
-}
-
-fn calculate_load(platform: &[[char; N]; N], max_x: usize, max_y: usize) -> usize {
-    let mut load = 0;
-    for y in 0..=max_y {
-        for x in 0..=max_x {
-            if platform[y][x] == 'O' {
-                load += max_y - y + 1;
-            }
-        }
-    }
-    load
 }
 
 fn tilt_north(platform: &mut [[char; N]; N], max_x: usize, max_y: usize) {
@@ -78,52 +65,23 @@ fn tilt_north(platform: &mut [[char; N]; N], max_x: usize, max_y: usize) {
     }
 }
 
-fn tilt_west(platform: &mut [[char; 100]; 100], max_x: usize, max_y: usize) {
-    for x in 0..=max_x {
-        for y in 0..=max_y {
-            if platform[y][x] == 'O' {
-                for dx in 1..=x {
-                    if platform[y][x - dx] != '.' {
-                        break;
-                    }
-                    platform[y][x - dx] = 'O';
-                    platform[y][x - dx + 1] = '.';
-                }
-            }
-        }
-    }
+fn calculate_load(platform: &[[char; N]; N], max_y: usize) -> usize {
+    platform.iter()
+        .take(max_y + 1)
+        .enumerate()
+        .map(|(y, row)| row.iter().filter(|&&c| c == 'O').count() * (max_y - y + 1))
+        .sum()
 }
 
-fn tilt_south(platform: &mut [[char; N]; N], max_x: usize, max_y: usize) {
-    for y in (0..=max_y).rev() {
-        for x in 0..=max_x {
-            if platform[y][x] == 'O' {
-                for dy in 1..=(max_y - y) {
-                    if platform[y + dy][x] != '.' {
-                        break;
-                    }
-                    platform[y + dy][x] = 'O';
-                    platform[y + dy - 1][x] = '.';
-                }
-            }
+fn rotate(platform: &[[char; 100]; 100], max_x: usize, max_y: usize) -> [[char; N]; N] {
+    // rotate platform clockwise, returning a new platform
+    let mut new_platform = [[' '; N]; N];
+    for (y, row) in platform.iter().enumerate().take(max_y + 1) {
+        for (x, &cell) in row.iter().enumerate().take(max_x + 1) {
+            new_platform[x][max_y - y] = cell;
         }
     }
-}
-
-fn tilt_east(platform: &mut [[char; 100]; 100], max_x: usize, max_y: usize) {
-    for x in (0..=max_x).rev() {
-        for y in 0..=max_y {
-            if platform[y][x] == 'O' {
-                for dx in 1..=(max_x - x) {
-                    if platform[y][x + dx] != '.' {
-                        break;
-                    }
-                    platform[y][x + dx] = 'O';
-                    platform[y][x + dx - 1] = '.';
-                }
-            }
-        }
-    }
+    new_platform
 }
 
 #[cfg(test)]
