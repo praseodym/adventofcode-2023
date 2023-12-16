adc56adaa5bfbcf3044cf95affefea6f24d8117b
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-16 06:49:24 +0100

    Day 16: part two

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,9 +1,10 @@
+use std::cmp;
 use std::collections::HashSet;
 
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 const N: usize = 111;
@@ -18,10 +19,37 @@ fn run(input: &'static str) -> (usize, usize) {
         len = y + 1;
     }
 
+    let initial = ((1, 1), (1, 0));
+    let part1_answer = simulate(&contraption, len, initial);
+
+    let mut part2_answer = 0;
+    for x in 0..=len {
+        part2_answer = cmp::max(part2_answer, simulate(&contraption, len, ((x, 1), (0, 1))));
+        part2_answer = cmp::max(
+            part2_answer,
+            simulate(&contraption, len, ((x, len), (0, -1))),
+        );
+    }
+    for y in 0..=len {
+        part2_answer = cmp::max(part2_answer, simulate(&contraption, len, ((1, y), (1, 0))));
+        part2_answer = cmp::max(
+            part2_answer,
+            simulate(&contraption, len, ((len, y), (-1, 0))),
+        );
+    }
+
+    (part1_answer, part2_answer)
+}
+
+fn simulate(
+    contraption: &[[char; 111]; 111],
+    len: usize,
+    initial: ((usize, usize), (isize, isize)),
+) -> usize {
     let mut queue: Vec<((usize, usize), (isize, isize))> = Vec::new();
     let mut seen: HashSet<((usize, usize), (isize, isize))> = HashSet::new();
     let mut energized: HashSet<(usize, usize)> = HashSet::new();
-    queue.push(((1, 1), (1, 0)));
+    queue.push(initial);
     while let Some(((x, y), (dx, dy))) = queue.pop() {
         if x == 0 || y == 0 || x > len || y > len {
             continue;
@@ -75,9 +103,7 @@ fn run(input: &'static str) -> (usize, usize) {
         energized.insert((x, y));
     }
 
-    let part1_answer = energized.len();
-    let mut part2_answer = 0;
-    (part1_answer, part2_answer)
+    energized.len()
 }
 
 #[cfg(test)]
@@ -86,15 +112,15 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 7482);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 7896);
     }
 
     #[test]
     fn test_input_example() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 46);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 51);
     }
 }
