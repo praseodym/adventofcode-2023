7db4b4cc968a050d534ed4ca30318777f4ed5702
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-19 23:11:05 +0100

    Day 19: cleanup

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -6,76 +6,104 @@ fn main() {
     println!("part 2 answer: {}", part2_answer);
 }
 
+type Workflows<'a> = HashMap<&'a str, Vec<Rule>>;
+type Part = [usize; 4];
+type Parts = [[usize; 4]];
+type Action = &'static str;
+type Paths = Vec<Vec<(usize, usize)>>;
+
+enum Rule {
+    Comparison {
+        idx: usize,
+        gt: bool,
+        num: usize,
+        action: Action,
+    },
+    Action(Action),
+}
+
 fn run(input: &'static str) -> (usize, usize) {
-    let mut s = input.split("\n\n");
-    let mut workflows = HashMap::new();
-    for line in s.next().unwrap().lines() {
-        let (name, rules) = line.split_once('{').unwrap();
-        let rules: Vec<&str> = rules.strip_suffix('}').unwrap().split(',').collect();
-        workflows.insert(name, rules);
-    }
-    let parts = s
-        .next()
-        .unwrap()
-        .lines()
-        .map(|line| {
-            line.strip_prefix('{')
-                .unwrap()
-                .strip_suffix('}')
-                .unwrap()
-                .split(',')
-                .map(|s| {
-                    let (_, v) = s.split_once('=').unwrap();
-                    v.parse::<usize>().unwrap()
-                })
-                .collect::<Vec<usize>>()
-        })
-        .collect::<Vec<_>>();
-
+    let (workflows, parts) = parse_input(input);
     let part1_answer = evaluate_parts(&workflows, &parts);
-
-    let result = acceptable_ranges(&workflows, workflows.get("in").unwrap());
-    let part2_answer: usize = result
+    let part2_answer = acceptable_ranges(&workflows, workflows.get("in").unwrap())
         .iter()
         .map(|r| r.iter().fold(1, |acc, (min, max)| acc * (max - min + 1)))
         .sum();
-
     (part1_answer, part2_answer)
 }
 
-fn evaluate_parts(workflows: &HashMap<&str, Vec<&str>>, parts: &[Vec<usize>]) -> usize {
-    let mut part1_answer = 0;
+fn parse_input(input: &'static str) -> (Workflows, Vec<Part>) {
+    let (w, p) = input.split_once("\n\n").unwrap();
+
+    let mut workflows: Workflows = HashMap::new();
+    for line in w.lines() {
+        let (name, rules) = line.split_once('{').unwrap();
+        let rules: Vec<Rule> = rules
+            .strip_suffix('}')
+            .unwrap()
+            .split(',')
+            .map(|s| {
+                if let Some((c, action)) = s.split_once(':') {
+                    let var = c.chars().next().unwrap();
+                    let idx = "xmas".chars().position(|c| c == var).unwrap();
+                    let op = c.chars().nth(1).unwrap();
+                    let gt = op == '>';
+                    let num = c[2..].parse::<usize>().unwrap();
+                    Rule::Comparison {
+                        idx,
+                        gt,
+                        num,
+                        action,
+                    }
+                } else {
+                    Rule::Action(s)
+                }
+            })
+            .collect();
+        workflows.insert(name, rules);
+    }
+
+    let parts = p
+        .lines()
+        .map(|line| {
+            let mut res = [0; 4];
+            for (n, v) in res.iter_mut().zip(line[1..line.len() - 1].split(',')) {
+                let (_, v) = v.split_once('=').unwrap();
+                *n = v.parse::<usize>().unwrap();
+            }
+            res
+        })
+        .collect::<Vec<_>>();
+
+    (workflows, parts)
+}
+
+fn evaluate_parts(workflows: &Workflows, parts: &Parts) -> usize {
+    let mut ratings_sum = 0;
     'parts: for part in parts {
         let mut rules = workflows.get("in").unwrap();
         'workflow: loop {
             for rule in rules {
-                let action = if let Some((check, action)) = rule.split_once(':') {
-                    let var = check.chars().next().unwrap();
-                    let op = check.chars().nth(1).unwrap();
-                    let num = check[2..].parse::<usize>().unwrap();
-                    let var = match var {
-                        'x' => part[0],
-                        'm' => part[1],
-                        'a' => part[2],
-                        's' => part[3],
-                        _ => unreachable!(),
-                    };
-                    let result = match op {
-                        '>' => var > num,
-                        '<' => var < num,
-                        _ => unreachable!(),
-                    };
-                    if result {
-                        Some(action)
-                    } else {
-                        None
+                let action = match *rule {
+                    Rule::Comparison {
+                        idx,
+                        gt,
+                        num,
+                        action,
+                    } => {
+                        let var = part[idx];
+                        let result = if gt { var > num } else { var < num };
+                        if result {
+                            Some(action)
+                        } else {
+                            None
+                        }
                     }
-                } else {
-                    Some(*rule)
+                    Rule::Action(a) => Some(a),
                 };
                 if let Some(action) = action {
                     if action == "A" {
-                        part1_answer += part.iter().sum::<usize>();
+                        ratings_sum += part.iter().sum::<usize>();
                         continue 'parts;
                     } else if action == "R" {
                         continue 'parts;
@@ -87,76 +115,58 @@ fn evaluate_parts(workflows: &HashMap<&str, Vec<&str>>, parts: &[Vec<usize>]) ->
             }
         }
     }
-    part1_answer
+    ratings_sum
 }
 
-fn acceptable_ranges(
-    workflows: &HashMap<&str, Vec<&str>>,
-    rules: &[&str],
-) -> Vec<Vec<(usize, usize)>> {
-    let rule = rules[0];
-    let a = if let Some((check, action)) = rule.split_once(':') {
-        let var = check.chars().next().unwrap();
-        let op = check.chars().nth(1).unwrap();
-        let num = check[2..].parse::<usize>().unwrap();
-        let var = match var {
-            'x' => 0usize,
-            'm' => 1usize,
-            'a' => 2usize,
-            's' => 3usize,
-            _ => unreachable!(),
-        };
-        let gt = match op {
-            '>' => true,
-            '<' => false,
-            _ => unreachable!(),
-        };
-        let num_acceptable = match op {
-            '>' => num + 1,
-            '<' => num - 1,
-            _ => unreachable!(),
-        };
-        let mut a = constrain_paths(var, gt, num, acceptable_ranges(workflows, &[action]));
-        let b = constrain_paths(
-            var,
-            !gt,
-            num_acceptable,
-            acceptable_ranges(workflows, &rules[1..]),
-        );
-        a.extend(b);
-        a
-    } else if rule == "A" {
-        vec![vec![(1, 4000usize); 4]]
-    } else if rule == "R" {
-        vec![]
-    } else {
-        acceptable_ranges(workflows, workflows.get(rule).unwrap())
-    };
-    a
-}
-
-fn constrain_paths(
-    var: usize,
-    gt: bool,
-    num: usize,
-    paths: Vec<Vec<(usize, usize)>>,
-) -> Vec<Vec<(usize, usize)>> {
-    let mut new_ranges = vec![];
-    for r in &paths {
-        let mut n = r.clone();
-        let (mut min, mut max) = r[var];
-        if gt {
-            min = min.max(num + 1);
-        } else {
-            max = max.min(num - 1);
+fn acceptable_ranges(workflows: &Workflows, rules: &[Rule]) -> Vec<Vec<(usize, usize)>> {
+    let rule = &rules[0];
+    match *rule {
+        Rule::Action("A") => vec![vec![(1, 4000); 4]],
+        Rule::Action("R") => vec![],
+        Rule::Action(action) => acceptable_ranges(workflows, workflows.get(action).unwrap()),
+        Rule::Comparison {
+            idx,
+            gt,
+            num,
+            action,
+        } => {
+            let mut a = constrain_paths(
+                idx,
+                gt,
+                num,
+                acceptable_ranges(workflows, &[Rule::Action(action)]),
+            );
+            let b = constrain_paths(
+                idx,
+                !gt,
+                if gt { num + 1 } else { num - 1 },
+                acceptable_ranges(workflows, &rules[1..]),
+            );
+            a.extend(b);
+            a
         }
-        if min > max {
-            continue;
-        }
-        n[var] = (min, max);
-        new_ranges.push(n)
     }
-    new_ranges
+}
+
+fn constrain_paths(idx: usize, gt: bool, num: usize, paths: Paths) -> Paths {
+    paths
+        .iter()
+        .flat_map(|r| {
+            let mut r = r.clone();
+            let (mut min, mut max) = r[idx];
+            if gt {
+                min = min.max(num + 1);
+            } else {
+                max = max.min(num - 1);
+            }
+            if min > max {
+                None
+            } else {
+                r[idx] = (min, max);
+                Some(r)
+            }
+        })
+        .collect()
 }
 
 #[cfg(test)]
