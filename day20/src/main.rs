3ed4e7c9cf9a6e192dc805e0d6a57fa487145cc0
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-20 23:56:56 +0100

    Day 20: part two

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -8,36 +8,48 @@ fn main() {
 
 fn run(input: &'static str) -> (usize, usize) {
     let mut modules = parse_input(input);
-    let (low_count, high_count) = simulate(&mut modules);
+    let (low_count, high_count, rx_iters) = simulate(&mut modules);
 
     let part1_answer = low_count * high_count;
-    let part2_answer = 0;
+    let part2_answer = rx_iters;
     (part1_answer, part2_answer)
 }
 
-fn simulate(modules: &mut HashMap<&'static str, Module>) -> (usize, usize) {
+fn simulate(modules: &mut HashMap<&'static str, Module>) -> (usize, usize, usize) {
     let mut pulses = VecDeque::new();
     let (mut low_count, mut high_count) = (0, 0);
 
-    for i in 1..=1000 {
-        println!("Button press {}", i);
-        println!("  Broadcasting from button");
+    let rx_input = modules.values().find(|m| m.outputs.contains(&"rx"));
+    let mut rx_state = if let Some(rx_input) = rx_input {
+        let rx_input_name = rx_input.name;
+        let rx_input_inputs_count = rx_input.inputs.len();
+        let rx_periods: HashMap<&str, usize> = HashMap::with_capacity(rx_input_inputs_count);
+        Some((rx_input_name, rx_input_inputs_count, rx_periods))
+    } else {
+        println!("no rx module found, no answer for part 2");
+        None
+    };
+
+    let mut button_presses = 0;
+    'button_presses: loop {
+        button_presses += 1;
         pulses.push_back(Pulse {
             high: false,
             from: "button",
             to: "broadcaster",
         });
         while let Some(pulse) = pulses.pop_front() {
-            if pulse.high {
-                high_count += 1;
-            } else {
-                low_count += 1;
+            if button_presses <= 1000 {
+                if pulse.high {
+                    high_count += 1;
+                } else {
+                    low_count += 1;
+                }
+            } else if rx_state.is_none() {
+                break 'button_presses;
             }
-            println!("  Processing pulse: {:?}", pulse);
-            println!("  hi: {}, lo: {}", high_count, low_count);
             let module = modules.get_mut(pulse.to);
             if module.is_none() {
-                println!("    No module found for pulse: {:?}", pulse);
                 continue;
             }
             let module = module.unwrap();
@@ -45,24 +57,31 @@ fn simulate(modules: &mut HashMap<&'static str, Module>) -> (usize, usize) {
                 ModuleKind::Broadcaster => Some(pulse.high),
                 ModuleKind::FlipFlop { ref mut state } => {
                     if !pulse.high {
-                        println!("    Flipping flipflop state: {}", state);
                         *state = !*state;
-                        //module.kind = ModuleKind::FlipFlop { state };
                         Some(*state)
                     } else {
                         None
                     }
                 }
                 ModuleKind::Conjunction { ref mut state } => {
+                    if let Some((rx_input_name, rx_input_inputs_count, ref mut rx_periods)) =
+                        rx_state
+                    {
+                        if module.name == rx_input_name {
+                            if pulse.high && !rx_periods.contains_key(pulse.from) {
+                                rx_periods.insert(pulse.from, button_presses);
+                            }
+                        } else if rx_periods.len() == rx_input_inputs_count {
+                            break 'button_presses;
+                        }
+                    }
                     if pulse.high {
                         state.insert(pulse.from, true);
                     } else {
                         state.insert(pulse.from, false);
                     }
-                    println!("    Conjunction state: {:?}", state);
                     Some(!state.values().all(|&v| v))
                 }
-                ModuleKind::None => unreachable!(),
             };
             if let Some(new_pulse) = new_pulse {
                 for output in &module.outputs {
@@ -71,27 +90,38 @@ fn simulate(modules: &mut HashMap<&'static str, Module>) -> (usize, usize) {
                         from: module.name,
                         to: output,
                     };
-                    println!("    Sending pulse: {:?}", p);
                     pulses.push_back(p);
                 }
             }
-            println!("    Saved state: {:?}", modules.get(pulse.to).unwrap().kind);
-
-            // if high_count > 100 {
-            //     panic!("too many pulses");
-            // }
         }
     }
 
-    println!("low_count: {}", low_count);
-    println!("high_count: {}", high_count);
-
-    (low_count, high_count)
+    let rx_iters = if let Some((_, _, rx_periods)) = rx_state {
+        lcm(&rx_periods.values().copied().collect::<Vec<_>>()[..])
+    } else {
+        0
+    };
+    (low_count, high_count, rx_iters)
 }
 
-// fn send_pulse(modules: &HashMap<&'static str, Module>) -> Vec<Pulse {
-//
-// }
+fn gcd(a: usize, b: usize) -> usize {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
+fn lcm(v: &[usize]) -> usize {
+    let mut result = v[0];
+    for &i in &v[1..] {
+        result = result * i / gcd(result, i);
+    }
+    result
+}
 
 fn parse_input(input: &'static str) -> HashMap<&'static str, Module> {
     let mut modules = HashMap::new();
@@ -112,10 +142,12 @@ fn parse_input(input: &'static str) -> HashMap<&'static str, Module> {
         let module = Module {
             kind,
             name,
+            inputs: Vec::new(),
             outputs,
         };
         modules.insert(name, module);
     }
+
     let mut module_inputs: HashMap<&str, Vec<&str>> = HashMap::new();
     for module in modules.values() {
         for output in &module.outputs {
@@ -124,15 +156,17 @@ fn parse_input(input: &'static str) -> HashMap<&'static str, Module> {
         }
     }
 
-    println!("module_inputs: {:?}", module_inputs);
     for module in modules.values_mut() {
-        if let ModuleKind::Conjunction { ref mut state } = module.kind {
-            for output in module_inputs.get(module.name).unwrap().iter() {
-                state.insert(output, false);
+        if let Some(inputs) = module_inputs.remove(module.name) {
+            module.inputs = inputs;
+            if let ModuleKind::Conjunction { ref mut state } = module.kind {
+                for output in &module.inputs {
+                    state.insert(output, false);
+                }
             }
-            println!("module: {:?}", module);
         }
     }
+
     modules
 }
 
@@ -147,6 +181,7 @@ struct Pulse {
 struct Module {
     kind: ModuleKind,
     name: &'static str,
+    inputs: Vec<&'static str>,
     outputs: Vec<&'static str>,
 }
 
@@ -155,7 +190,6 @@ enum ModuleKind {
     Broadcaster,
     FlipFlop { state: bool },
     Conjunction { state: HashMap<&'static str, bool> },
-    None,
 }
 
 #[cfg(test)]
@@ -164,22 +198,20 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 825167435, "incorrect part 1 answer");
-        // assert_eq!(part2_answer, 0, "incorrect part 2 answer");
+        assert_eq!(part2_answer, 225514321828633, "incorrect part 2 answer");
     }
 
     #[test]
     fn test_input_example1() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example1"));
+        let (part1_answer, _) = run(include_str!("../input-example1"));
         assert_eq!(part1_answer, 32000000, "incorrect part 1 answer");
-        // assert_eq!(part2_answer, 0, "incorrect part 2 answer");
     }
 
     #[test]
     fn test_input_example2() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example2"));
+        let (part1_answer, _) = run(include_str!("../input-example2"));
         assert_eq!(part1_answer, 11687500, "incorrect part 1 answer");
-        // assert_eq!(part2_answer, 0, "incorrect part 2 answer");
     }
 }
