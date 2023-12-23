89cd2b16d6f65c7de80355194944df53dab29f76
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-23 16:24:05 +0100

    Day 23: part two

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -19,38 +19,46 @@ fn run(input: &'static str) -> (usize, usize) {
     assert_eq!(grid[start.1][start.0], '.', "start must be '.'");
     assert_eq!(grid[end.1][end.0], '.', "end must be '.'");
 
-    let mut max_steps = 0;
-    let mut stack = Vec::new();
-    stack.push((start, 0, [[false; N]; N]));
-    while let Some(((x, y), steps, mut visited)) = stack.pop() {
-        visited[y][x] = true;
-        if (x, y) == end {
-            if steps > max_steps {
-                max_steps = steps;
-            }
-            continue;
-        }
-        let neighbours = match grid[y][x] {
-            '#' => continue,
-            '.' => vec![(-1, 0), (1, 0), (0, -1), (0, 1)],
-            '>' => vec![(1, 0)],
-            '<' => vec![(-1, 0)],
-            '^' => vec![(0, -1)],
-            'v' => vec![(0, 1)],
-            _ => unreachable!(),
-        };
-        for (dx, dy) in neighbours {
-            let (nx, ny) = (x.saturating_add_signed(dx), y.saturating_add_signed(dy));
-            if visited[ny][nx] || grid[ny][nx] == '#' {
-                continue;
-            }
-            stack.push(((nx, ny), steps + 1, visited));
-        }
+    let part1_answer = dfs(&grid, start, end, 0, &mut [[false; N]; N], false);
+    let part2_answer = dfs(&grid, start, end, 0, &mut [[false; N]; N], true);
+    (part1_answer, part2_answer)
+}
+
+const NEIGHBOURS: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+fn dfs(
+    grid: &[[char; N]; N],
+    (x, y): (usize, usize),
+    end: (usize, usize),
+    steps: usize,
+    visited: &mut [[bool; N]; N],
+    part2: bool,
+) -> usize {
+    if (x, y) == end {
+        return steps;
     }
 
-    let part1_answer = max_steps;
-    let part2_answer = 0;
-    (part1_answer, part2_answer)
+    let neighbours = match grid[y][x] {
+        _ if part2 => &NEIGHBOURS,
+        '.' => &NEIGHBOURS,
+        '<' => &NEIGHBOURS[0..1],
+        '>' => &NEIGHBOURS[1..2],
+        '^' => &NEIGHBOURS[2..3],
+        'v' => &NEIGHBOURS[3..4],
+        _ => unreachable!(),
+    };
+
+    let mut max = 0;
+    visited[y][x] = true;
+    for &(dx, dy) in neighbours {
+        let (nx, ny) = (x.saturating_add_signed(dx), y.saturating_add_signed(dy));
+        if visited[ny][nx] || grid[ny][nx] == '#' {
+            continue;
+        }
+        max = max.max(dfs(grid, (nx, ny), end, steps + 1, visited, part2));
+    }
+    visited[y][x] = false;
+    max
 }
 
 #[cfg(test)]
@@ -59,15 +67,15 @@ mod tests {
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 2386, "incorrect part 1 answer");
-        // assert_eq!(part2_answer, 0, "incorrect part 2 answer");
+        assert_eq!(part2_answer, 6246, "incorrect part 2 answer");
     }
 
     #[test]
     fn test_input_example() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example"));
+        let (part1_answer, part2_answer) = run(include_str!("../input-example"));
         assert_eq!(part1_answer, 94, "incorrect part 1 answer");
-        // assert_eq!(part2_answer, 0, "incorrect part 2 answer");
+        assert_eq!(part2_answer, 154, "incorrect part 2 answer");
     }
 }
