c75ca6858680b1524b9fa44be3dd2e989a325c3a
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-12-24 00:36:17 +0100

    Day 23: optimise part two

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
@@ -14,22 +16,77 @@ fn run(input: &'static str) -> (usize, usize) {
             grid[y][x] = c;
         });
     });
-    let start = (1, 0);
-    let end = (len - 2, len - 1);
-    assert_eq!(grid[start.1][start.0], '.', "start must be '.'");
-    assert_eq!(grid[end.1][end.0], '.', "end must be '.'");
 
-    let part1_answer = dfs(&grid, start, end, 0, &mut [[false; N]; N], false);
-    let part2_answer = dfs(&grid, start, end, 0, &mut [[false; N]; N], true);
+    let part1_answer = solve(&grid, len, false);
+    let part2_answer = solve(&grid, len, true);
     (part1_answer, part2_answer)
 }
 
 const NEIGHBOURS: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
 
+type Point = (usize, usize);
+type Neighbour = ((usize, usize), usize);
+
+fn solve(grid: &[[char; N]; N], len: usize, part2: bool) -> usize {
+    let start = (1, 0);
+    let end = (len - 2, len - 1);
+    assert_eq!(grid[start.1][start.0], '.', "start must be '.'");
+    assert_eq!(grid[end.1][end.0], '.', "end must be '.'");
+
+    // approach heavily inspired by https://github.com/AxlLind/AdventOfCode2023/blob/main/src/bin/23.rs
+    let mut graph = HashMap::<Point, Vec<Neighbour>>::new();
+    for x in 0..len {
+        for y in 0..len {
+            let neighbours = match grid[y][x] {
+                '#' => continue,
+                _ if part2 => &NEIGHBOURS,
+                '.' => &NEIGHBOURS,
+                '<' => &NEIGHBOURS[0..1],
+                '>' => &NEIGHBOURS[1..2],
+                '^' => &NEIGHBOURS[2..3],
+                'v' => &NEIGHBOURS[3..4],
+                _ => unreachable!(),
+            };
+            let e = graph.entry((x, y)).or_default();
+            for &(dx, dy) in neighbours {
+                let (nx, ny) = (x as isize + dx, y as isize + dy);
+                if nx < 0 || nx >= len as isize || ny < 0 || ny >= len as isize {
+                    continue;
+                }
+                let (nx, ny) = (nx as usize, ny as usize);
+                if grid[ny][nx] == '#' {
+                    continue;
+                }
+                e.push(((nx, ny), 1));
+            }
+        }
+    }
+
+    let corridors: Vec<_> = graph
+        .iter()
+        .filter_map(|(k, v)| if v.len() == 2 { Some(*k) } else { None })
+        .collect();
+
+    for point in corridors {
+        let neighbours = graph.remove(&point).unwrap();
+        let [(p1, d1), (p2, d2)] = neighbours.as_slice().try_into().unwrap();
+        let n1 = graph.get_mut(&p1).unwrap();
+        if let Some(n) = n1.iter_mut().find(|(p, _)| *p == point) {
+            *n = (p2, d1 + d2);
+        }
+        let n2 = graph.get_mut(&p2).unwrap();
+        if let Some(n) = n2.iter_mut().find(|(p, _)| *p == point) {
+            *n = (p1, d1 + d2);
+        }
+    }
+
+    dfs(&graph, start, end, 0, &mut [[false; N]; N], part2)
+}
+
 fn dfs(
-    grid: &[[char; N]; N],
-    (x, y): (usize, usize),
-    end: (usize, usize),
+    graph: &HashMap<Point, Vec<Neighbour>>,
+    (x, y): Point,
+    end: Point,
     steps: usize,
     visited: &mut [[bool; N]; N],
     part2: bool,
@@ -38,24 +95,13 @@ fn dfs(
         return steps;
     }
 
-    let neighbours = match grid[y][x] {
-        _ if part2 => &NEIGHBOURS,
-        '.' => &NEIGHBOURS,
-        '<' => &NEIGHBOURS[0..1],
-        '>' => &NEIGHBOURS[1..2],
-        '^' => &NEIGHBOURS[2..3],
-        'v' => &NEIGHBOURS[3..4],
-        _ => unreachable!(),
-    };
-
     let mut max = 0;
     visited[y][x] = true;
-    for &(dx, dy) in neighbours {
-        let (nx, ny) = (x.saturating_add_signed(dx), y.saturating_add_signed(dy));
-        if visited[ny][nx] || grid[ny][nx] == '#' {
+    for &((nx, ny), d) in graph[&(x, y)].iter() {
+        if visited[ny][nx] {
             continue;
         }
-        max = max.max(dfs(grid, (nx, ny), end, steps + 1, visited, part2));
+        max = max.max(dfs(graph, (nx, ny), end, steps + d, visited, part2));
     }
     visited[y][x] = false;
     max
